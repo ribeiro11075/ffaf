beeb208b903d88efb30583138d69499f0d890bf0
ubuntu <ribeiro11075@gmail.com>
2020-07-09 19:17:02 -0700

    Added MVC project structure

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Baby Girl Helder');
+  res.send('Hello Baby Girl Test');
 });
 
 app.listen(PORT, HOST);
